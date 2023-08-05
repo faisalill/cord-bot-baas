aa3d02a0ff1bbe46340556ad2985a4e8920715fc
faisalill <mfaisal.ilyas.d@gmail.com>
2023-08-05 08:14:00 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,107 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { RetrievalQAChain, loadQAStuffChain } from "langchain/chains";
+import {appwriteDocsStore, utopiaPhpDocStore} from './vector_dance.js';
+import { PromptTemplate } from 'langchain/prompts';
+import { OpenAI } from 'langchain/llms/openai';
+
+dotenv.config()
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+
+const model = new OpenAI({
+  openAIApiKey: OPENAI_API_KEY,
+  modelName: "gpt-3.5-turbo",
+  temperature: 0,
+})
+
+let retrievedAppwriteVectorStore = false
+let appwriteVectorStore = null
+let appwriteChain = null
+let retrievedUtopiaPhpVectorStore = false
+let utopiaPhpVectorStore = null
+let utopiaPhpChain = null
+
+async function loadVectorStores() {
+  
+  const promptTemplate = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+       You are a chatbot for a backend as a service app called appwrite. I have fed documents using langchain try to use them to answer the questions .node-appwrite is node.js server sdk and appwrite is the javascript sdk. 
+
+       Question: {question}`;
+  
+       const prompt = PromptTemplate.fromTemplate(promptTemplate);
+
+       const utopiaPromptTemplate = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+        You are a chatbot for utopia-php a php framework. I have fed documents using langchain try to use them to answer the questions .utopia-php has mutiple frameworks under it.
+
+       Question: {question}`;
+  
+       const utopiaPrompt = PromptTemplate.fromTemplate(promptTemplate);
+  
+  appwriteDocsStore.then((vectorStore) => {
+    appwriteVectorStore = vectorStore
+    retrievedAppwriteVectorStore = true
+    appwriteChain = new RetrievalQAChain({
+      combineDocumentsChain: loadQAStuffChain(model, { prompt }),
+      retriever: appwriteVectorStore.asRetriever(),
+    });
+    console.log("Appwrite vector store loaded")
+  })
+
+  utopiaPhpDocStore.then((vectorStore)=>{
+    utopiaPhpVectorStore = vectorStore
+    retrievedUtopiaPhpVectorStore = true
+    utopiaPhpChain = new RetrievalQAChain({
+      combineDocumentsChain: loadQAStuffChain(model, { utopiaPrompt }),
+      retriever: utopiaPhpVectorStore.asRetriever(),
+    });
+    console.log("Utopia PHP vector store loaded")
+  })
+
+}
+
+loadVectorStores()
+
+const app = express()
+app.use(express.json())
+
+app.post('/appwrite', async (req, res) => {
+  const question = req.body.question
+  if (retrievedAppwriteVectorStore) {
+    await appwriteChain.call({
+      query: question,
+    }).then((answer)=>{
+    res.send(answer);
+    })
+  }
+  else {
+    res.send("Appwrite vector store not loaded yet")
+  }
+
+})
+
+app.post('/utopia-php', async (req, res) => {
+  const question = req.body.question
+  if (retrievedUtopiaPhpVectorStore) {
+    await utopiaPhpChain.call({
+      query: question,
+    }).then((answer)=>{
+    res.send(answer);
+    console.log(answer.text);
+    }
+    )
+  }
+  else {
+    res.send("Utopia Vector Store not loaded yet")
+  }
+
+})
+
+const port = process.env.PORT || 8182
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+}
+)
---
+++ vector_dance.js
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv'
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+dotenv.config()
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+
+const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: OPENAI_API_KEY,
+})
+
+const appwriteDocsStore = new Promise((resolve, reject) => {
+    const loader = new TextLoader("./appwrite-docs/appwriteDocs.txt");
+loader.load().then(async (data) => {
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        chunkOverlap: 0,
+    });
+    textSplitter.splitDocuments(data).then(async(documents)=>{
+        let appwriteStore = await MemoryVectorStore.fromDocuments(documents, embeddings);
+        resolve(appwriteStore);
+      })
+})
+})
+
+const utopiaPhpDocStore = new Promise ((resolve, reject)=>{
+    const loader = new TextLoader("./utopia-php/utopia-php.md")
+    loader.load().then(async(data)=>{
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 500,
+            chunkOverlap: 0,
+        });
+        textSplitter.splitDocuments(data).then(async(documents)=>{
+            let utopiaPhpStore = await MemoryVectorStore.fromDocuments(documents, embeddings);
+            resolve(utopiaPhpStore);
+        })
+    })
+})
+
+export { appwriteDocsStore, utopiaPhpDocStore}
